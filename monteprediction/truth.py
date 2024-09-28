bcffb27d2ce688220c54db2572d8228ff4bba77b
EndCar808 <endacar808@gmail.com>
2024-09-28 17:48:29 +0200

    Added verification script

--- monteprediction/truth.py
+++ monteprediction/truth.py
@@ -39,6 +39,12 @@ def get_previous_tuesday(date):
     previous_tuesday = date - timedelta(days=days_to_subtract)
     return previous_tuesday
 
+def get_most_recent_truth():
+    end_date = back_to_weekday(datetime.now() - timedelta(days=1))
+    start_date = end_date - timedelta(weeks=3)
+    recent_data = yf.download(SPDR_ETFS, start=start_date.date(), end=end_date.date(), interval="1wk")
+    recent_weekly_prices = recent_data['Adj Close']
+    return recent_weekly_prices.pct_change().dropna().iloc[-1].values
 
 def get_truth(expiry):
     """
---
+++ verify.py
@@ -0,0 +1,17 @@
+from monteprediction.truth import get_most_recent_truth
+from monteprediction.scoring import compute_score
+
+from monteprediction.verification import get_verification_status
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+print(get_verification_status(email=os.getenv("YOUR_EMAIL")))
+
+
+z = get_most_recent_truth()
+print({'z':z})
+
+# score = compute_score(samples=df.values, z=z)
+# print(f"Score using your samples above last week: {score}")
